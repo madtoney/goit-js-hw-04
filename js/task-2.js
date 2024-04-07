bd1b823b1a34e5cfb25e9922d74a73bdaee8a1fa
Dmytro <95138887+madtoney@users.noreply.github.com>
2024-04-07 22:41:14 +0300

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const universalArray = firstArray.concat(secondArray);
+  if (universalArray.length >= maxLength) {
+    return universalArray.slice(0, maxLength);
+  } else {
+    return universalArray;
+  }
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
